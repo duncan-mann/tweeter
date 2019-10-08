b7c6e19deca5499fcf85f6ae9fa874033bf9c351
Duncan Mann <duncan.s.mann@gmail.com>
2019-10-08 21:43:17 +0000

    Made a function within app.js that makes creation of tweets dynamic. Changed hover function within tweet-hover.js so that this functionality works for all rendered tweets.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,4 +3,68 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready(function() {
+
+let user = {
+
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+};
+
+{/* <article class="tweet">
+    <header>
+        <img src='../images/avatars/066-Spiderman.png'>
+        <h4>Spider-Man</h4>
+      </header>
+        <p>Spider-men just wanna have fun</p>
+        <footer>
+        <p>10 days ago</p>
+        <div>
+          <img src='../images/icons/like.png'>
+          <img src='../images/icons/flag.png'>
+          <img src='../images/icons/repeat.png'>
+        </div>
+    </footer>
+  </article> */}
+ 
+
+createTweetElement = function(user) {
+ let $article = `<article class="tweet ${user.user.name}">
+ <header>
+      <img src=${user.user.avatars}>
+      <h4>${user.user.name}</h4>
+      <p class="username">${user.user.handle}</p>
+    </header>
+      <p>${user.content.text}</p>
+      <footer>
+      <p>${user.created_at}</p>
+      <div>
+        <img src='../images/icons/like.png'>
+        <img src='../images/icons/flag.png'>
+        <img src='../images/icons/repeat.png'>
+      </div>
+  </footer>
+</article>`;
+
+return $article;
+}
+
+
+let article = createTweetElement(user);
+// console.log(article);
+let section = document.getElementsByClassName('new-tweet');
+$(section).append(article);
+
+
+
+});
+
+
 
--- public/scripts/tweet-hover.js
+++ public/scripts/tweet-hover.js
@@ -1,15 +1,25 @@
 $(document).ready(function() {
-  let tweets = document.getElementsByClassName('tweet');
-  tweets = tweets[0];
-  let user = $('<p>')
-  $(user).text('@notpeterparker');
-  $(user).addClass('userHandle');
 
-  $('.tweet').hover( () => {
-    $('.tweet').addClass('shadow');
-    $('.tweet').find('header').append(user);
-    }, () => {
-    $('.tweet').removeClass('shadow');
-    $(user).remove();
-  })
+  $('.tweet').on('mouseover', function() {
+    $(this).children('header').children('.username').css({ display: 'inline' });
+    $(this).addClass('shadow');
+  });
+  $('.tweet').on('mouseleave', function() {
+    $(this).children('header').children('.username').css({ display: 'none' });
+    $(this).removeClass('shadow');
+  });
+  
 })
+
+// OLD WAY (didn't work)
+  // let username = $('<p>');
+  // $(username).text('@notpeterparker');
+  // $(username).addClass('userHandle');
+
+  // $('.tweet').hover( () => {
+  //   $('.tweet').addClass('shadow');
+  //   $('.tweet').find('header').append(username);
+  //   }, () => {
+  //   $('.tweet').removeClass('shadow');
+  //   $(username).remove();
+  // })
