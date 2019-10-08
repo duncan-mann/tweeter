943771b4b8d1676067f55a905ff897b4dfc23ca9
Duncan Mann <duncan.s.mann@gmail.com>
2019-10-08 22:53:09 +0000

    Fixed z-index of navbar so it remains in front of other elements

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,36 +5,32 @@
  */
 $(document).ready(function() {
 
-let user = {
 
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-};
-
-{/* <article class="tweet">
-    <header>
-        <img src='../images/avatars/066-Spiderman.png'>
-        <h4>Spider-Man</h4>
-      </header>
-        <p>Spider-men just wanna have fun</p>
-        <footer>
-        <p>10 days ago</p>
-        <div>
-          <img src='../images/icons/like.png'>
-          <img src='../images/icons/flag.png'>
-          <img src='../images/icons/repeat.png'>
-        </div>
-    </footer>
-  </article> */}
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
  
-
 createTweetElement = function(user) {
  let $article = `<article class="tweet ${user.user.name}">
  <header>
@@ -56,12 +52,15 @@ createTweetElement = function(user) {
 return $article;
 }
 
+const renderTweets = function(tweets) {
+  for (each of tweets) {
+    let article = createTweetElement(each);
+    let section = document.getElementsByClassName('new-tweet');
+    $(section).append(article);
+  }
+}
 
-let article = createTweetElement(user);
-// console.log(article);
-let section = document.getElementsByClassName('new-tweet');
-$(section).append(article);
-
+renderTweets(data);
 
 
 });
