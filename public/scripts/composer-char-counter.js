ed0befd7882bfa3d7fd89d155af34a592da87c42
Duncan Mann <duncan.s.mann@gmail.com>
2019-10-08 15:25:12 +0000

    Many changes since last commit. Styled several elements on the page. Added a counter to the add tweet block using jQuery. Added template block for how tweets appear - still in production.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+
+  $('#textArea').on('keyup', function() {
+    let textArea = jQuery(this);
+    let textAreaLength = textArea.context.textLength;
+    let counter = $(this).next().find('span');
+    counter.text(140 - textAreaLength);
+
+    if (textAreaLength > 140) {
+      counter.text('-' + (textAreaLength - 140));
+      $(counter).css({
+        'color': 'red'
+      });
+    } else {
+      $(counter).css({
+        'color': '#545149'
+      });
+    }
+  })
+
+ 
+});
